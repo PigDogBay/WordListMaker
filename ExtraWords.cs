68d02c05e491b3774fe99c12d2a5c567e9a75234
mark <mpdbailey.technology@gmail.com>
2019-06-19 10:52:31 +0100

    Added extra words from user feedback: snowpea, doner, lusophone, therm, monetarise

--- ExtraWords.cs
+++ ExtraWords.cs
@@ -26,7 +26,7 @@ namespace WordListMaker
         };
         public static List<String> feedback = new List<String>(){
                  "rulings", "ecarte", "sashimi", "leangle", "neddy", "actioned", "nidor", "manspreading", "covfefe", "trenchcoat", "actioned", "hutia", "roedean", "onesie",
-                 "twerk","twerking","twerks", "umbrellabird", "weei", "smokie", "zol", "piert", "goodfella", "goodfellas"
+                 "twerk","twerking","twerks", "umbrellabird", "weei", "smokie", "zol", "piert", "goodfella", "goodfellas", "snowpea", "doner", "lusophone", "therm", "monetarise"
         };
         public static List<String> neoligism = new List<String>(){
                 "onesie", "covfefe", "fatberg"
