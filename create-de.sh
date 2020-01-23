0679b06146ec9d3214b8a498cec2f6ff0215bfd6
mark <mpdbailey.technology@gmail.com>
2020-01-23 16:31:36 +0000

    Added comments about how the script works

--- create-de.sh
+++ create-de.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
+#Install the german word list using
+#sudo apt-get install aspell-de
+#
+#The dictionary can be dumped out and all the declensions/conjugations expanded to create the base word list
+#The conjugations/declensions are spearated by spaces so these need to be converted to newlines
+#
+#Next I remove the acronyms, the regex checks if second or last letter is capitalized and if so removes it
+#This will remove most acronyms including SuSE VMware CoE and leave words like McDonald untouched.
+#- sed "s/match pattern/substitute string/g"
+#- \<\w[A-Z]\w*\>
+#-- \< start of word
+#-- \w any word character
+#-- [A-Z] any capital letter
+#-- \w* any word character 0 or more times
+#-- \> end of word
+#- \<\w\+[A-Z]\>  = start of word + any letters + capital letter + end of word
+#- /^$/d = this deletes the empty lines left by the removal of the acronym
+#
+# Now that the acronyms have been removed I can convert to lower case
+# gawk tolower() converts accented letters to lower case as well as A-Z
+#
+# Convert äöüß to ae, oe, ue, ss using sed's substition function
+#
+# iconv converts accented chars to normal a-z chars
+#
+# sed delete function to remove words of 2 letters or less
+#
+# Sort the words by length and then alphabetically
+#- gawk is used to print the length and word
+#- sort field 1 numerically and reverse, field 2 alphabetically
+#- cut uses space as a delimiter and selects only the second field to the end of the line
+#- uniq removes repeated adjacent lines
+#
+# The two letter word lists is a based on the Aspell-de list but each word has been checked by Google translate
 echo "Creating wordlist-de.txt"
 aspell -d de dump master \
 | aspell -l de expand \
