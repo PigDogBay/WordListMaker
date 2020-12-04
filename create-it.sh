ad7d8320854ebc035e200951f3efb7d61166ee4d
mark <mpdbailey.technology@gmail.com>
2020-12-04 10:55:16 +0000

    Adding comments for italian script

--- create-it.sh
+++ create-it.sh
@@ -5,9 +5,19 @@
 #The dictionary can be dumped out and all the declensions/conjugations expanded to create the base word list
 #The conjugations/declensions are spearated by spaces so these need to be converted to newlines
 #
+#Remove Roman numerals
+#Before splitting the lines up I can remove huge lines of Roman numerals
+#sed "/[a-z\s]*iii[a-z\s]*/d"   - \s space, this looks for iii in a line
+#
+#Split lines up into  1 word per line
+#tr ' ' '\n'
+#
+#Remove words with apostrtophes, look like compounded words
+#sed "/^.*'.*$/d"   ^ start of line, .* any character 1 or more times, $ end of line
+#
 #Next I remove the acronyms, the regex checks if second or last letter is capitalized and if so removes it
 #This will remove most acronyms including SuSE VMware CoE and leave words like McDonald untouched.
-#- sed "s/match pattern/substitute string/g"
+#- sed "/match pattern/d" - delete mataching pattern
 #- \<\w[A-Z]\w*\>
 #-- \< start of word
 #-- \w any word character
@@ -15,7 +25,6 @@
 #-- \w* any word character 0 or more times
 #-- \> end of word
 #- \<\w\+[A-Z]\>  = start of word + any letters + capital letter + end of word
-#- /^$/d = this deletes the empty lines left by the removal of the acronym
 #
 # Now that the acronyms have been removed I can convert to lower case
 # gawk tolower() converts accented letters to lower case as well as A-Z
@@ -31,7 +40,7 @@
 #- uniq removes repeated adjacent lines
 #
 # The two letter word lists is a based on the Aspell-es list but each word has been checked by Google translate and Collins
-# https://www.collinsdictionary.com/dictionary/italian-english/hola
+# https://www.collinsdictionary.com/dictionary/italian-english/ciao
 #
 
 echo "Creating wordlist-it.txt"
