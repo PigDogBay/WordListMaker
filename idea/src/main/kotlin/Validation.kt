04c1759f4302ffc61bfd3d4578f71d5fd1cf680e
Mark Bailey <markpdbailey@gmail.com>
2020-12-14 11:19:01 +0000

    Validation check for illegal chars

--- idea/src/main/kotlin/Validation.kt
+++ idea/src/main/kotlin/Validation.kt
@@ -1,4 +1,16 @@
+import java.io.File
+
 /**
  * Only a-z chars allowed in a word
  */
 fun containsIllegalChar(word : String) : Boolean = word.any{it < 'a' || it > 'z'}
+
+
+fun validate(filename : String) : Int {
+    val invalidWords = File(filename)
+        .readLines()
+        .filter { containsIllegalChar(it) }
+
+         invalidWords.forEach { println(it) }
+        return invalidWords.count()
+}
--- idea/src/main/kotlin/main.kt
+++ idea/src/main/kotlin/main.kt
@@ -4,4 +4,7 @@ fun main(args: Array<String>) {
     val words = createScowlWordList()
     println("Count: ${words.count()}")
     words.saveWordList(OUT_FILENAME)
+    println("Validating - find any illegal words:")
+    val badWordCount = validate(OUT_FILENAME)
+    println("Found $badWordCount illegal words")
 }
