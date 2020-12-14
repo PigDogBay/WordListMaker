0145175800049001843e617a049332d89ceb99e8
Mark Bailey <markpdbailey@gmail.com>
2020-12-14 11:47:55 +0000

    Roman numerals code

---
+++ idea/src/main/kotlin/RomanNumerals.kt
@@ -0,0 +1,34 @@
+import java.util.*
+
+/**
+ * Based on
+ * https://stackoverflow.com/questions/12967896/converting-integers-to-roman-numerals-java
+ */
+class RomanNumerals {
+    private val numeralsMap = TreeMap<Int, String>()
+    init {
+        numeralsMap[1000] = "M"
+        numeralsMap[900] = "CM"
+        numeralsMap[500] = "D"
+        numeralsMap[400] = "CD"
+        numeralsMap[100] = "C"
+        numeralsMap[90] = "XC"
+        numeralsMap[50] = "L"
+        numeralsMap[40] = "XL"
+        numeralsMap[10] = "X"
+        numeralsMap[9] = "IX"
+        numeralsMap[5] = "V"
+        numeralsMap[4] = "IV"
+        numeralsMap[1] = "I"
+    }
+
+    fun toRoman(number : Int) : String {
+        //Find the key less than or equal to the number
+        val l = numeralsMap.floorKey(number)
+        if (number == l){
+            return numeralsMap[number]!!
+        }
+        return numeralsMap[l]!! + toRoman(number - l)
+    }
+
+}
---
+++ idea/src/test/kotlin/RomanNumeralsTest.kt
@@ -0,0 +1,35 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class RomanNumeralsTest {
+
+    @Test
+    fun toRoman1() {
+        val target = RomanNumerals()
+        assertEquals("MMXX",target.toRoman(2020))
+        assertEquals("MCMLXXXIV",target.toRoman(1984))
+        assertEquals("MCMXCIX",target.toRoman(1999))
+    }
+    @Test
+    fun toRoman2() {
+        val target = RomanNumerals()
+        assertEquals("III",target.toRoman(3))
+        assertEquals("VIII",target.toRoman(8))
+        assertEquals("XIII",target.toRoman(13))
+        assertEquals("XV",target.toRoman(15))
+        assertEquals("XVII",target.toRoman(17))
+        assertEquals("XVIII",target.toRoman(18))
+        assertEquals("XX",target.toRoman(20))
+        assertEquals("XXI",target.toRoman(21))
+        assertEquals("XXII",target.toRoman(22))
+        assertEquals("XXIII",target.toRoman(23))
+        assertEquals("XXIV",target.toRoman(24))
+        assertEquals("XXV",target.toRoman(25))
+        assertEquals("XXVI",target.toRoman(26))
+        assertEquals("XXVII",target.toRoman(27))
+        assertEquals("XXVIII",target.toRoman(28))
+        assertEquals("XXIX",target.toRoman(29))
+        assertEquals("XXX",target.toRoman(30))
+    }
+}
