ebd7a3da155d213c33c8d93e816d174160f6641f
Mark Bailey <markpdbailey@gmail.com>
2021-01-06 11:19:16 +0000

    Validation code for Scowl, de,fr,es,it and pt word lists - now run from a main function

--- idea/src/main/kotlin/Validation.kt
+++ idea/src/main/kotlin/Validation.kt
@@ -1,5 +1,30 @@
 import java.io.File
 
+const val SCOWL_FILENAME = "../../out/words.txt"
+const val PORTUGUESE_FILENAME = "../../out/wordlist-pt.txt"
+const val FRENCH_FILENAME = "../../out/wordlist-fr.txt"
+const val SPANISH_FILENAME = "../../out/wordlist-es.txt"
+const val GERMAN_FILENAME = "../../out/wordlist-de.txt"
+const val ITALIAN_FILENAME = "../../out/wordlist-it.txt"
+
+
+fun main(args: Array<String>) {
+    validatePrinter("SCOWL", SCOWL_FILENAME)
+    validatePrinter("German",GERMAN_FILENAME )
+    validatePrinter("Spanish", SPANISH_FILENAME)
+    validatePrinter("French", FRENCH_FILENAME)
+    validatePrinter("Italian", ITALIAN_FILENAME)
+    validatePrinter("Portuguese", PORTUGUESE_FILENAME)
+}
+
+private fun validatePrinter(displayName : String, filename: String){
+    println("   ***   $displayName   ***")
+    val errors = validate(filename)
+    println("$displayName error count $errors")
+    println("-----------------------")
+}
+
+
 /**
  * Only a-z chars allowed in a word
  */
