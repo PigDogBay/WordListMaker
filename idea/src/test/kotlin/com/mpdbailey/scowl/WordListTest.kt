f3ed931fb72f62f1e29a747df1b0594e167233fd
Mark Bailey <markpdbailey@gmail.com>
2021-03-05 11:12:54 +0000

    removeAccents() - added extra accented letters

--- idea/src/test/kotlin/com/mpdbailey/scowl/WordListTest.kt
+++ idea/src/test/kotlin/com/mpdbailey/scowl/WordListTest.kt
@@ -22,4 +22,22 @@ internal class WordListTest {
         assertEquals("mary had a little lamb", "máry hád ä littlê lämb".removeAccents())
         assertEquals("all's well that ends well", "all's well that ends well".removeAccents())
     }
+    /**
+     * Accents found in UKACD
+     */
+    @Test
+    fun removeAccents3(){
+        assertEquals("AAoiIOEEaEoU","ÀÅòìÎÖÉÉãÉòÜ".removeAccents())
+    }
+    /**
+     * From
+     * https://stackoverflow.com/questions/20690499/concrete-javascript-regex-for-accented-characters-diacritics
+     * àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅå
+     */
+    @Test
+    fun removeAccents4(){
+        assertEquals("aeiouAEIOUaeiouyAEIOUYaeiouAEIOUanoANOaeiouyAEIOUYcCOoAa",
+            "àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇØøÅå".removeAccents())
+    }
+
 }
