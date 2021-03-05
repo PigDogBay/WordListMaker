6e9c90af1273d7ae8e95ecff326dfcc8526fb7a6
Mark Bailey <markpdbailey@gmail.com>
2021-03-05 17:14:32 +0000

    Added code to generate phrases from UKACD

--- idea/src/main/kotlin/com/mpdbailey/scowl/Ukacd.kt
+++ idea/src/main/kotlin/com/mpdbailey/scowl/Ukacd.kt
@@ -4,8 +4,11 @@ import com.mpdbailey.utils.removeAccents
 
 class Ukacd(private val filename : String) {
     private val regexIllegal = Regex("[\\s'\\-.!]")
+    private val regexPhrases = Regex("[\\s\\-]")
+    private val regexAccents = Regex("[À-ÖØ-öø-ÿ]")
+    private val regexNonAZ = Regex("[^a-zA-Z]")
 
-    fun createWordList() : List<String>{
+    fun singleWords() : List<String>{
         return loadWordList(filename)
             .asSequence()
             .filter {!it.contains(regexIllegal)}
@@ -13,6 +16,30 @@ class Ukacd(private val filename : String) {
             .map { it.removeAccents() }
             .sortedWith(comparator.thenBy { it })
             .distinct()
+            .toList()
+    }
+
+    private fun compressString(str : String) : String{
+        return str.replace(",","")
+            .replace(",","")
+            .replace("?","")
+            .replace("!","")
+            .replace("'","")
+            .replace(".","")
+            .replace(";","")
+            .replace(":","")
+            .replace(" ","")
+            .replace("-","")
+    }
+
+    fun phrases() : List<Pair<String,String>>{
+        return loadWordList(filename)
+            .asSequence()
+            .filter {it.contains(regexPhrases)}
+            .map { it.removeAccents() }
+            .sortedWith(comparator.thenBy { it })
+            .distinct()
+            .map{Pair(compressString(it).toLowerCase(),it)}
             .toList()
     }
 }
