a1e8ea54000802f0b282beb0c9c055f180e20840
Mark Bailey <markpdbailey@gmail.com>
2021-03-16 16:49:45 +0000

    Code to create compressed nabu DB

--- idea/src/main/kotlin/main.kt
+++ idea/src/main/kotlin/main.kt
@@ -1,3 +1,6 @@
+import com.mpdbailey.nabu.Combine
+import com.mpdbailey.nabu.Compressor
+import com.mpdbailey.nabu.DatabaseLookup
 import com.mpdbailey.scowl.*
 import com.mpdbailey.utils.saveWordList
 import com.mpdbailey.utils.removeWordSeparators
@@ -9,14 +12,19 @@ const val UKACD17_FILENAME = "../../wordlists/UKACD/UKACD17.TXT"
 
 fun createNabuDb(){
     println("Creating Nabu database")
+    val combine = Combine()
+    val compressor = Compressor(combine.indices(),combine.synonyms())
+    println("Compressing indices")
+    val compIndices = compressor.compressedIndices
+    println("Compressing sets")
+    val compSets = compressor.compressedSynonymSets
     val database = com.mpdbailey.nabu.Database(NABU_FILENAME)
-    val combine = com.mpdbailey.nabu.Combine()
     println("Creating Db")
     database.create()
     println("Inserting Indices")
-    database.insert(combine.indices())
+    database.insert(compIndices)
     println("Inserting Definitions")
-    database.insertSynonyms(combine.synonyms())
+    database.insertSynonyms(compSets)
 }
 
 fun createScowl(){
@@ -44,9 +52,14 @@ fun createPhrases(){
     println("Phrases Count: ${phrases.count()}")
 }
 
+fun dbLookup(query : String){
+    val dbl = DatabaseLookup(NABU_FILENAME)
+    val r = dbl.search(query)
+    r.forEach { println(it) }
+    println("Count ${r.count()}")
+}
 
 fun main(args: Array<String>) {
-//    createNabuDb()
-    createScowl()
-    createPhrases()
+    createNabuDb()
+    dbLookup("close")
 }
