52f5304f806c4968f4c9e2d984b86fbccc9da2fa
Mark Bailey <markpdbailey@gmail.com>
2021-03-16 14:51:59 +0000

    Clean all synonym index words

--- idea/src/main/kotlin/com/mpdbailey/nabu/Compressor.kt
+++ idea/src/main/kotlin/com/mpdbailey/nabu/Compressor.kt
@@ -3,13 +3,37 @@ package com.mpdbailey.nabu
 class Compressor(indices : List<SynonymIndex>, synonymSets : List<SynonymSet>) {
 
     private val compressedIndex = CompressedIndex()
+    private val regexIllegal = Regex("[./0-9]")
 
     init {
         compressedIndex.createMap(synonymSets.map{it.index})
     }
 
+    private fun cleanIndexWord(word : String) = word
+        .replace("\'","")
+        .replace('_',' ')
+        .replace('-',' ')
+
+    private fun compressMultipleIndices(indices : List<SynonymIndex>) : SynonymIndex {
+        val all = indices
+            .flatMap { it.indices }
+            .distinct()
+        return SynonymIndex(indices[0].word, all)
+    }
+
     val compressedIndices =
-        indices.map { SynonymIndex(it.word,compressedIndex.compress(it.indices)) }
+        indices
+            .asSequence()
+            .filter { it.word.length in 2..28 }
+            .filter {!it.word.contains(regexIllegal)}
+            .filter {!(it.word.contains('\'') && it.word.length<7)}
+            .map { SynonymIndex(
+                cleanIndexWord(it.word)
+                ,compressedIndex.compress(it.indices)) }
+            //multiple entries for can exist for the same phrase, eg step-in or 'step in'
+            .groupBy { it.word }
+            .map { compressMultipleIndices(it.value) }
+            .toList()
 
     val compressedSynonymSets = synonymSets.map{ SynonymSet(
             compressedIndex.compress(it.index),
