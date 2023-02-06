0b40c166e23bfe8624752ebc3f3297ee17fcb484
Mark Bailey <markpdbailey@gmail.com>
2023-02-06 16:20:33 +0000

    Moved validation checks into unit tests

--- idea/src/test/kotlin/wordnet.common/DbFileHelperTest.kt
+++ idea/src/test/kotlin/wordnet.common/DbFileHelperTest.kt
@@ -45,4 +45,49 @@ class DbFileHelperTest {
         val path =  target.getDbFilePath(DbType.Data,DbPartOfSpeech.Adj)
         assertTrue(File(path).exists())
     }
+
+    /**
+    Check that the indices have the correct part of speech
+     */
+    private fun checkPartOfSpeech(dbPartOfSpeech: DbPartOfSpeech) : Boolean {
+        val indices = DbFileHelper().loadIndices(dbPartOfSpeech)
+        return indices.none { it.partOfSpeech != dbPartOfSpeech.letter }
+    }
+
+    /**
+    Check that the definitions have the correct part of speech
+     */
+    private fun checkDefPartOfSpeech(dbPartOfSpeech: DbPartOfSpeech) : Boolean {
+        val definitions = DbFileHelper().loadDefinitions(dbPartOfSpeech)
+        return definitions
+            .none { it.partOfSpeech != dbPartOfSpeech.letter}
+    }
+
+    @Test
+    fun partOfSpeechNoun(){
+        assertTrue(checkPartOfSpeech(DbPartOfSpeech.Noun))
+        assertTrue(checkDefPartOfSpeech(DbPartOfSpeech.Noun))
+    }
+    @Test
+    fun partOfSpeechVerb(){
+        assertTrue(checkPartOfSpeech(DbPartOfSpeech.Verb))
+        assertTrue(checkDefPartOfSpeech(DbPartOfSpeech.Verb))
+    }
+    @Test
+    fun partOfSpeechAdv(){
+        assertTrue(checkPartOfSpeech(DbPartOfSpeech.Adv))
+        assertTrue(checkDefPartOfSpeech(DbPartOfSpeech.Adv))
+    }
+
+    /**
+     * Adjective letter can be a or s (satellite adjective)
+     */
+    @Test
+    fun partOfSpeechAdj(){
+        assertTrue(checkPartOfSpeech(DbPartOfSpeech.Adj))
+        val definitions = DbFileHelper().loadDefinitions(DbPartOfSpeech.Adj)
+        val actual = definitions
+            .none { !DbPartOfSpeech.isAdjective(it.partOfSpeech)}
+        assertTrue(actual)
+    }
 }
